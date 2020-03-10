afcf786721f8c7da12068fe214bcca217ad7c2e7
ValleTSF <valletsf@gmail.com>
2020-03-10 14:15:17 +0100

    No duplicates in cart

--- index.js
+++ index.js
@@ -19,33 +19,37 @@ $(document).ready(function() {
             <div class="amount">0</div>
             <button class="plusbtn">+</button>
         </div>`;
-       });
-
-
-
-
-       const cartexpanded = document.getElementById("cartexpanded");
-       $(".plusbtn").click(function() {
-        let x = document.getElementById("productinfo").childNodes[9];
-         console.log(x.innerHTML);
-         let x2 = parseInt(x.innerHTML);
-   
-         x.innerHTML = x2 + 1;
-   
-         cartexpanded.innerHTML += this.parentNode.innerHTML;
-   
-         console.log(this.parentNode.innerHTML);
-         if (cartexpanded.innerHTML == this.parentNode.innerHTML) {
-           console.log("plus amount");
-         } else {
-           cartexpanded.innerHTML += this.parentNode.innerHTML;
-           console.log(cartexpanded);
-         }
-       });
-
-
-      
     });
 
+    const cartexpanded = document.getElementById("cartexpanded");
+    let cartArray = [];
+    $(".plusbtn").click(function() {
+      let i = this.parentNode;
+      let x = i.childNodes[9];
+      let v = cartexpanded.childNodes[13];
 
+      if (
+        cartexpanded.innerHTML.includes(this.parentNode.childNodes[1].innerHTML)
+      ) {
+
+
+        let v2 = parseInt(v.innerHTML);
+        v.innerHTML = v2 + 1;
+        let x2 = parseInt(x.innerHTML);
+        x.innerHTML = x2 + 1;
+      } else {
+        cartArray.push(this.parentNode);
+        console.log(cartArray);
+        
+        cartexpanded.innerHTML += this.parentNode.innerHTML;
+        console.log(cartexpanded.innerHTML);
+        console.log(this.parentNode.childNodes[1]);
+
+        console.log(x.innerHTML);
+        let x2 = parseInt(x.innerHTML);
+
+        x.innerHTML = x2 + 1;
+      }
+    });
   });
+});
