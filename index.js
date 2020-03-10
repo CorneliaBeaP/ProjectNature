fb6c6f474226278553672d07cdd6d7cb4dcbaf4b
ValleTSF <valletsf@gmail.com>
2020-03-10 12:52:39 +0100

    Staged changes

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-
-
-
-
-
-
-$(document).ready(function(){
-
-
-
-
-
+$(document).ready(function() {
   const productbox = document.getElementById("productbox");
-    $("#cart").click(function(){
-      $("#cartexpanded").toggle();
-    });
-    $.getJSON("products.json", function(response) {
-     console.log(response)
-     productArray = response.products;
-     console.log(productArray);
-       productArray.forEach(element => {
-        productbox.innerHTML += ` <div class="product">
+  $("#cart").click(function() {
+    $("#cartexpanded").toggle();
+  });
+  $.getJSON("products.json", function(response) {
+    console.log(response);
+    productArray = response.products;
+    console.log(productArray);
+    productArray.forEach(element => {
+      productbox.innerHTML += ` <div class="product">
         <img src="${element.imageLink}" alt="">
         <div id="productinfo">
             <div class="productname">${element.name}</div>
@@ -29,69 +18,29 @@ $(document).ready(function(){
             <button class="minusbtn">-</button>
             <div class="amount">0</div>
             <button class="plusbtn">+</button>
-        </div>`
-       });
-
-
-
-
-const cartexpanded = document.getElementById("cartexpanded");
-       $(".plusbtn").click(function() {
-let x = document.getElementById("productinfo").childNodes[9];
-console.log(x.innerHTML);
-let x2 = parseInt(x.innerHTML);
-
-x.innerHTML = x2 + 1;
-
-
-
-
-
-
-       cartexpanded.innerHTML += this.parentNode.innerHTML;
-
-       
-       });
-
-
-
-
-
+        </div>`;
     });
 
+    const cartexpanded = document.getElementById("cartexpanded");
+    $(".plusbtn").click(function() {
+      let x = document.getElementById("productinfo").childNodes[9];
+      console.log(x.innerHTML);
+      let x2 = parseInt(x.innerHTML);
 
+      x.innerHTML = x2 + 1;
 
+      cartexpanded.innerHTML += this.parentNode.innerHTML;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      console.log(this.parentNode.innerHTML);
+      if (cartexpanded.innerHTML == this.parentNode.innerHTML) {
+        console.log("plus amount");
+      } else {
+        cartexpanded.innerHTML += this.parentNode.innerHTML;
+        console.log(cartexpanded);
+      }
+    });
   });
-
-
-
-
-
-
-
-
+});
 
 // var i;
 
@@ -103,6 +52,6 @@ x.innerHTML = x2 + 1;
 //       cartexpanded.style.maxHeight = null;
 //     } else {
 //       cartexpanded.style.maxHeight = cartexpanded.scrollHeight + "px";
-//     } 
+//     }
 //   });
 // }
