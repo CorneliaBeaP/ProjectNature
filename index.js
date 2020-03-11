cb20fc3cfadc65603be29645c121b92bc07afc37
Cornelia Bea Persson <corneliap@live.se>
2020-03-11 21:21:14 +0100

    cart showing right amount, removal if below 1, clone of product

--- index.js
+++ index.js
@@ -15,18 +15,24 @@ $(document).ready(function () {
       <div class="productname">${element.name}</div>
       <div class="description">${element.description}
       </div>
-      <div class="price">Pris ${element.price} kr</div>
+      <div class="priceinfo">Pris <div class="price">${element.price}</div> kr</div>
       <button class="minusbtn">-</button>
       <div class="amount">0</div>
       <button class="plusbtn">+</button>
       </div>`;
 
     });
-
+    let cloneamount;
     const cartexpanded = document.getElementById("cartexpanded");
     const cartproductbox = document.getElementById("cartproductbox");
-    let newArray = [];
-    let cartexpandedArray=[];
+
+    let cartexpandedArray = [];
+    let clonearray = [];
+    tempArray = [];
+
+
+
+
     $(".plusbtn").click(function () {
       let i = this.parentNode;
       let x = i.childNodes[9];
@@ -35,40 +41,46 @@ $(document).ready(function () {
       x.innerHTML = x2 + 1;
       let arrayitem;
 
-let pname;
-let pdescription;
-let pprice;
-let pamount;
+      let pname;
+      let pdescription;
+      let pprice;
+      let pamount;
 
+      let sum = document.getElementById("sum");
+      let sumprice = i.childNodes[5].childNodes[1].innerHTML;
+      let sumprice2 = parseInt(sumprice);
 
+      console.log(sumprice2);
+
+      let sum2 = parseInt(sum.innerHTML);
+      sum.innerHTML = sum2 + sumprice2 + "kr";
 
 
       if (
         !cartproductbox.innerHTML.includes(this.parentNode.childNodes[1].innerHTML)
       ) {
-        
+        let clone = $(this.parentNode).clone();
+        clone.appendTo("#cartproductbox");
+        clonearray.push(clone);
 
 
 
-
-        cartproductbox.innerHTML += this.parentNode.innerHTML;
 
       } else {
 
 
 
-        let v2 = parseInt(cartproductbox.childNodes[9].innerHTML);
+        clonearray.forEach(element => {
 
+          if (element[0].childNodes[1].innerHTML.includes(this.parentNode.childNodes[1].innerHTML)) {
+            cloneamount = parseInt(element[0].childNodes[9].innerHTML);
+            element[0].childNodes[9].innerHTML = cloneamount + 1;
+          }
 
-
-        cartproductbox.childNodes[9].innerHTML = v2 + 1;
+        });
 
 
       }
-
-
-
-
 
       arrayitem = this.parentNode;
       pname = arrayitem.childNodes[1].innerHTML;
@@ -76,61 +88,89 @@ let pamount;
       pprice = arrayitem.childNodes[5].innerHTML;
       pamount = arrayitem.childNodes[9].innerHTML;
 
-if(newArray.includes(arrayitem.childNodes[1].innerHTML)){
-
-
-  let amountforproduct = parseInt(newArray[3]);
-
-  newArray[3] = amountforproduct + 1;
-
-
-
-}else{
- newArray=[];
-  newArray.push(pname, pdescription, pprice, pamount);
-cartexpandedArray.push(newArray);
-
-localStorage.setItem('cartexpandedArray', JSON.stringify(cartexpandedArray));
-
-console.log(cartexpandedArray);
-
-  cartArray.push(newArray);
-
-
-
-}
-
-
-
-
-
-
     });
 
 
     $(".minusbtn").click(function () {
+      // uppdaterar sidan
       let i = this.parentNode;
       let x = i.childNodes[9];
 
       let x2 = parseInt(x.innerHTML);
       if (x2 > 0)
         x.innerHTML = x2 - 1;
+
+      let sum = document.getElementById("sum");
+      let sumprice = i.childNodes[5].childNodes[1].innerHTML;
+      let sumprice2 = parseInt(sumprice);
+
+      console.log(sumprice2);
+
+      let sum2 = parseInt(sum.innerHTML);
+
+
+
+
+      // uppdaterar kundvagn
+      clonearray.forEach(element => {
+        cloneamount = parseInt(element[0].childNodes[9].innerHTML);
+        if (element[0].childNodes[9].innerHTML == 1 && element[0].childNodes[1].innerHTML.includes(this.parentNode.childNodes[1].innerHTML)) {
+          sum.innerHTML= 0 + "kr";
+          clonearray.splice(clonearray.indexOf(element), 1);
+          element[0].remove();
+
+        } else {
+          if (element[0].childNodes[1].innerHTML.includes(this.parentNode.childNodes[1].innerHTML)) {
+            element[0].childNodes[9].innerHTML = cloneamount - 1;
+            sum.innerHTML = sum2 - sumprice2 + "kr";
+          }
+
+
+        }
+      });
+
+
+
     });
 
     $("#clearcart").click(function () {
-      // töm varukorgen
+      cartproductbox.innerHTML = "";
+      clonearray = [];
+      cartArray = [];
+
+      let amountarray = Array.from(document.getElementsByClassName("amount"));
+
+      for (i = 0; i < amountarray.length; i++) {
+        amountarray[i].innerHTML = 0;
+      }
 
     });
 
+
+
+
     $("#order").click(function () {
-      
-      
-      
+      clonearray.forEach(element => {
+        tempArray = [];
+        let pname2 = element[0].childNodes[1].innerHTML;
+        let pdescription2 = element[0].childNodes[3].innerHTML;
+        let pprice2 = element[0].childNodes[5].innerHTML;
+        let pamount2 = element[0].childNodes[9].innerHTML;
+
+        tempArray.push(pname2, pdescription2, pprice2, pamount2);
+        cartArray.push(tempArray);
+      });
+
+
+
+
 
       localStorage.setItem('cartArray', JSON.stringify(cartArray));
 
-
+      console.log(clonearray);
       window.location.href = "confirmation.html";
+
+
       // töm varukorgen 
     });
 
